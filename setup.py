84886c315ecea9517f9cac02a11ce03549797966
jacobharsten <spela.cob@gmail.com>
2018-07-06 16:23:19 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pythontimes',
+      version='1.0',
+      description='test pythonfile',
+      author='Jacob Harsten',
+      author_email='jacob@harsten.com',
+      packages=['pythontimes'],
+      scripts = ['bin/pythontimes'],
+     )
