7e1f0008016172017d9568b5a242096e2b865f9f
jacobharsten <spela.cob@gmail.com>
2018-07-06 15:48:31 +0200

    Create pythontimes.py

---
+++ pythontimes.py
@@ -0,0 +1,16 @@
+import sys
+
+def printFunStuff():
+    var1 = raw_input("Please enter a number: ")
+    var2 = raw_input("Please enter another number: ")
+    #sum = (var1 * var2)
+
+    #print var1, "*", var2, "=", sum
+    if( var1.isdigit()):
+        sum = int(var1) * int(var2)
+        print var1, "*", var2, "=", sum
+    else:
+        print "You need to enter a number!"
+
+if __name__ == '__main__':
+    printFunStuff()
